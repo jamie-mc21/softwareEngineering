93a4789f83e7e2930c90506401c8e45894f5c68f
jamie-mc21 <78818762+jamie-mc21@users.noreply.github.com>
2021-10-11 19:24:32 +0100

    creation of unit test

---
+++ lcaPython/unitTest.py
@@ -0,0 +1,14 @@
+import unittest
+from lca import lca
+
+
+# numbers used for testing lca.py
+bst = Node(9)
+bst.insert(3)
+bst.insert(9)
+bst.insert(2)
+bst.insert(1)
+bst.insert(11)
+bst.insert(8)
+bst.insert(7)
+bst.insert(15)
