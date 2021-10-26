5207e53574fa26013347f0eb366a9502ca721b5c
jamie-mc21 <78818762+jamie-mc21@users.noreply.github.com>
2021-10-26 23:19:30 +0100

    lowest common ancestor and DAG

--- lcaJava/unitTest.java
+++ lcaJava/unitTest.java
@@ -4,68 +4,58 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 
+
 @RunWith(JUnit4.class)
-public class unitTestJava {
+public class LCATest {
+
+
 
 	@Test
-	public void emptyTest() {
-    lca <Integer, Integer> LCA = new lca<Integer, Integer>();
-    assertEquals("checking if equal");
-    assertFalse("checking if false");
-    assertEmpty("checking if null");
-    assertTrue("checking if true")
-  }
+	public void testLCA() {
+		LCA tree = new LCA;
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		tree.root.right.left = new Node(6);
+		tree.root.right.right = new Node(7);
+		assertEquals("LCA of 2 and 3: ", 1, tree.findLCA(2, 3).data);
+		assertEquals("LCA of 4 and 5: ", 2, tree.findLCA(4, 5).data);
+		assertEquals("LCA of 6 and 7: ", 3, tree.findLCA(6, 7).data);
+	}
+
+	@Test
+	public void testFalseNodes(){
+		LCA tree = new LCA();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		assertEquals("False nodes: ", null, tree.findLCA(4, 5));
+	}
+
+	@Test
+	public void testDirectedAcyclicGraph()
+	{
+		DirectedAcyclicGraph DirectedAcyclicGraph = new DirectedAcyclicGraph(10);
+		DirectedAcyclicGraph.addEdge(1, 3);
+		DirectedAcyclicGraph.addEdge(4, 5);
+		DirectedAcyclicGraph.addEdge(4, 6);
+		DirectedAcyclicGraph.addEdge(5, 6);
+		DirectedAcyclicGraph.addEdge(5, 7);
+
+	}
+
 
 @Test
-  public void existsTest() {
-		lca<Integer, Integer> LCA = new lca<Integer, Integer>();
-		assertFalse("Checking if any node exists in empty tree");
-  }
-
-  @Test
-public void createHeadTest(){
-  LowestCommonAncestor newHead = new LowestCommonAncestor();
-  lca.createHead(1,6);
-  assertEquals("Create a head", '1');
+public void addEdge()
+{
+	DirectedAcyclicGraph DirectedAcyclicGraph = new DirectedAcyclicGraph(10);
+	DirectedAcyclicGraph.addEdge(1,3);
 }
 
-  @Test
-  public void addNodeTest(){
-    lca <Integer, Integer> newNode = new lca<Integer, Integer>();
-		lca.addNode(1,1);
-    assertTrue("Checking if tree contains key ", lca.exists(1));
-    lca.addNode(null,  null);
-    assertFalse("Checking if tree contains a null key ", lca.exists(null));
-  }
-
-
-  @Test
-  public void findNodeTest(){
-    lca <Integer, Integer> findNode = new lca<Integer, Integer>();
-    lca.addNode(1, 1);
-    lca.addNode(2, 2);
-    lca.addNode(3, 3);
-    lca.addNode(4, 4);
-    lca.addNode(5, 5);
-  }
-
-  @Test
-  public void lcaTest(){
-    lca<Integer, Integer> LCA = new lca<Integer, Integer>();
-		lca.addNode(1, 1);
-		lca.addNode(5, 5);
-		lca.addNode(3, 3);
-		lca.addNode(4, 4);
-		lca.addNode(8, 8);
-		lca.addNode(6, 6);
-  }
-
-  @Test
-  public void printTest(){
-    lca<Integer, Integer> LCA = new lca<Integer, Integer>();
-    lca.addNode(1, 4);
-    lca.addNode(2, 2);
-    lca.addNode(3, 4);
-    lca.addNode(9, 2);
-    lca.addNode(1, 4);
-  }
+@Test(expected=Exception.class)
+public void testFalse()
+{
+			DirectedAcyclicGraph DirectedAcyclicGraph = new DirectedAcyclicGraph(-10);
+}
