2aca2986bd0e23be4d8537f83e521beb86cf0217
jamie-mc21 <78818762+jamie-mc21@users.noreply.github.com>
2021-10-26 23:20:02 +0100

    lowest common ancestor

--- lcaPython/lca.py
+++ lcaPython/lca.py
@@ -1,50 +1,42 @@
-# a binary tree node
-class TreeNode:
-   def __init__(self, data, left = None, right = None):
-      self.data = data
-      self.left = left
-      self.right = right
 
-def insert(temp,data):
-   que = []
-   que.append(temp)
-   while (len(que)):
-      temp = que[0]
-      que.pop(0)
-      if (not temp.left):
-         if data is not None:
-            temp.left = TreeNode(data)
-         else:
-            temp.left = TreeNode(0)
-         break
-      else:
-         que.append(temp.left)
-         if (not temp.right):
-            if data is not None:
-               temp.right = TreeNode(data)
-            else:
-               temp.right = TreeNode(0)
+class Node:
+
+    def __init__(self, key):
+        self.key =  key
+        self.left = None
+        self.right = None
+
+def findPath( root, path, k):
+
+    if root is None:
+        return False
+
+    path.append(root.key)
+
+
+    if root.key == k :
+        return True
+
+
+    if ((root.left != None and findPath(root.left, path, k)) or
+            (root.right!= None and findPath(root.right, path, k))):
+        return True
+
+
+    path.pop()
+    return False
+
+def findLCA(root, n1, n2):
+
+    path1 = []
+    path2 = []
+
+    if (not findPath(root, path1, n1) or not findPath(root, path2, n2)):
+        return -1
+
+    i = 0
+    while(i < len(path1) and i < len(path2)):
+        if path1[i] != path2[i]:
             break
-         else:
-            que.append(temp.right)
-
-def make_tree(elements):
-   Tree = TreeNode(elements[0])
-   for element in elements[1:]:
-      insert(Tree, element)
-   return Tree
-
-class Solution(object):
-   def lowestCommonAncestor(self, root, p, q):
-      if not root:
-         return None
-      if root.data == p or root.data ==q:
-         return root
-      left = self.lowestCommonAncestor(root.left, p, q)
-      right = self.lowestCommonAncestor(root.right, p, q)
-      if right and left:
-         return root
-      return right or left
-ob1 = Solution()
-tree = make_tree([3,5,1,6,2,0,8,None,None,7,4])
-print(ob1.lowestCommonAncestor(tree, 5, 1).data)
+        i += 1
+    return path1[i-1]
--- lcaPython/unitTest.py
+++ lcaPython/unitTest.py
@@ -1,2 +1,24 @@
 import unittest
-from lca import lca
+import LCA
+
+class LCATest(unittest.TestCase):
+
+    def testLCA(self):
+
+        self.root = LCA.Node(4)
+        self.root.left = LCA.Node(3)
+        self.root.right = LCA.Node(7)
+        self.root.left.left = LCA.Node(1)
+        self.root.left.right = LCA.Node(5)
+        self.root.right.left = LCA.Node(6)
+        self.root.right.right = LCA.Node(8)
+
+        def test_findPath(self):
+
+            self.assertEqual(lca.LCA(self.root, 3, 5),3 , "LCA(3,5 ) should be 3")
+            self.assertEqual(lca.LCA(self.root, 7,6 ),7 , "LCA(7,6 ) should be 7")
+            self.assertEqual(lca.LCA(self.root, 4,1 ), 4, "LCA(4,1 ) should be 4")
+            self.assertEqual(lca.LCA(self.root, 9,7 ), None, "LCA(9,7 ) should be None")
+
+            if __name__ == '__main__':
+                unittest.main()
